f9477a95b3b18e5a76a252ff1176e1064578d721
Giulia Sorbi <giulia.sorbi.91@gmail.com>
2017-06-14 17:41:59 +0200

    restyling

--- double.cpp
+++ double.cpp
@@ -4,16 +4,15 @@ using namespace std;
 double square(double x)
 {
     return x*x;
-
 }
 
 void print_square(double x)
 {
     cout<<"The square of "<<x<<" is "<<square(x)<<"\n";
 }
+
 int main()
 {
-    std::cout<<"Hello world!\n";
     print_square(5);
-
+    return 0;
 }
