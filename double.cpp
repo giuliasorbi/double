7625cfb64c4fc22bcf87a04a592c5a179d6f6947
Federico Frenguelli <synasius@gmail.com>
2017-06-14 18:00:43 +0200

    adjusted code style

--- double.cpp
+++ double.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 double square(double x)
 {
-    return x*x;
+    return x * x;
 }
 
 void print_square(double x)
 {
-    cout<<"The square of "<<x<<" is "<<square(x)<<"\n";
+    cout << "The square of " << x << " is " << square(x) << "\n";
 }
 
 int main()
